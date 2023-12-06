c1e7cc8b122c7992361cbe3f33867408060ed53f
rahatmahmud12 <rahatuli12slam@gmail.com>
2023-12-07 03:26:59 +0600

    aassignment3 sleepy mode is on

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+
+const app: Application = express();
+
+//parsers
+app.use(express.json());
+app.use(cors());
+
+// application routes
+// app.use('/api/v1', router);
+
+const test = async (req: Request, res: Response) => {
+  const a = 10;
+  res.send(a);
+};
+
+app.get('/', test);
+
+export default app;
